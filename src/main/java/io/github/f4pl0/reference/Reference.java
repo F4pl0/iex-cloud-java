306aa48298bcb811423d95a86544254ee1b50dba
f4 <straka.filip@gmail.com>
2023-10-30 15:40:41 +0100

    docs: changed reference docs to not feature <h2> tags

--- src/main/java/io/github/f4pl0/reference/Reference.java
+++ src/main/java/io/github/f4pl0/reference/Reference.java
@@ -29,9 +29,13 @@ public class Reference {
     private final ObjectMapper mapper = new ObjectMapper();
 
     /**
-     * <h2>Daily IEX Trading Symbols</h2>
-     * Returns an array of symbols the Investors Exchange supports for trading. Symbols may be added or removed by the
-     * Investors Exchange at any time.
+     * Daily IEX Trading Symbols
+     *
+     * <p>
+     *     Returns an array of symbols the Investors Exchange supports for trading. Symbols may be added or removed by
+     *     the Investors Exchange at any time.
+     * </p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_IEX_SYMBOLS#daily-iex-trading-symbols">IEX Cloud API</a>
      * @throws IOException If the request fails.
      * @return A list of IEXTradingSymbol objects.
@@ -45,8 +49,10 @@ public class Reference {
     }
 
     /**
-     * <h2>Forex Reference data</h2>
-     * Returns an array of the supported currencies and currency pairs.
+     * Forex Reference data
+     *
+     * <p>Returns an array of the supported currencies and currency pairs.</p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_FX#forex-reference-data">IEX Cloud API</a>
      * @throws IOException If the request fails.
      * @return An IEXForexReferenceData object.
@@ -62,8 +68,10 @@ public class Reference {
     }
 
     /**
-     * <h2>Futures Reference Data</h2>
-     * Returns an array of the supported futures contract symbols.
+     * Futures Reference Data
+     *
+     * <p>Returns an array of the supported futures contract symbols.</p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_FUTURES#futures-reference-data">IEX Cloud API</a>
      * @return A list of IEXFuturesContractSymbol objects.
      * @throws IOException If the request fails.
@@ -77,8 +85,10 @@ public class Reference {
     }
 
     /**
-     * <h2>Futures Reference Data</h2>
-     * Returns an array of the supported futures contract symbols.
+     * Futures Reference Data
+     *
+     * <p>Returns an array of the supported futures contract symbols.</p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_FUTURES#futures-reference-data">IEX Cloud API</a>
      * @param underlying The underlying symbol of the futures contract.
      * @return A list of IEXFuturesContractSymbol objects.
@@ -95,8 +105,10 @@ public class Reference {
     }
 
     /**
-     * <h2>Mutual Fund Reference Data</h2>
-     * Returns an array of the supported mutual fund symbols.
+     * Mutual Fund Reference Data
+     *
+     * <p>Returns an array of the supported mutual fund symbols.</p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_MUTUAL_FUNDS#mutual-fund-reference-data">IEX Cloud API</a>
      * @return A list of IEXMutualFundSymbol objects.
      * @throws IOException If the request fails.
@@ -110,8 +122,10 @@ public class Reference {
     }
 
     /**
-     * <h2>OTC Reference Data</h2>
-     * Returns an array of the supported over-the-counter (OTC) symbols.
+     * OTC Reference Data
+     *
+     * <p>Returns an array of the supported over-the-counter (OTC) symbols.</p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_OTC#otc-reference-data">IEX Cloud API</a>
      * @return A list of IEXOTCSymbol objects.
      * @throws IOException If the request fails.
@@ -125,8 +139,12 @@ public class Reference {
     }
 
     /**
-     * <h2>Options Reference Data</h2>
-     * Returns an object keyed by symbol with the value of each symbol being an array of available contract dates.
+     * Options Reference Data
+     *
+     * <p>
+     *     Returns an object keyed by symbol with the value of each symbol being an array of available contract dates.
+     * </p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_OPTIONS#options-reference-data">IEX Cloud API</a>
      * @return A Map of String keys and List of Date values.
      * @throws IOException
@@ -163,8 +181,12 @@ public class Reference {
     }
 
     /**
-     * <h2>Options Reference Data</h2>
-     * Returns an object keyed by symbol with the value of each symbol being an array of available contract dates.
+     * Options Reference Data
+     *
+     * <p>
+     *     Returns an object keyed by symbol with the value of each symbol being an array of available contract dates.
+     * </p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_OPTIONS#options-reference-data">IEX Cloud API</a>
      * @param underlying The underlying symbol of the options.
      * @return A List of Options for the given underlying symbols.
@@ -181,8 +203,12 @@ public class Reference {
     }
 
     /**
-     * <h2>Options Reference Data</h2>
-     * Returns an object keyed by symbol with the value of each symbol being an array of available contract dates.
+     * Options Reference Data
+     *
+     * <p>
+     *     Returns an object keyed by symbol with the value of each symbol being an array of available contract dates.
+     * </p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_OPTIONS#options-reference-data">IEX Cloud API</a>
      * @param underlying The underlying symbol of the options.
      * @param expirationDate The expiration date of the options.
@@ -204,8 +230,12 @@ public class Reference {
     }
 
     /**
-     * <h2>Options Reference Data</h2>
-     * Returns an object keyed by symbol with the value of each symbol being an array of available contract dates.
+     * Options Reference Data
+     *
+     * <p>
+     *     Returns an object keyed by symbol with the value of each symbol being an array of available contract dates.
+     * </p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_OPTIONS#options-reference-data">IEX Cloud API</a>
      * @param underlying List of underlying symbols of the options.
      * @return A List of Options for the given array of underlying symbols.
@@ -226,8 +256,12 @@ public class Reference {
     }
 
     /**
-     * <h2>Options Reference Data</h2>
-     * Returns an object keyed by symbol with the value of each symbol being an array of available contract dates.
+     * Options Reference Data
+     *
+     * <p>
+     *     Returns an object keyed by symbol with the value of each symbol being an array of available contract dates.
+     * </p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_OPTIONS#options-reference-data">IEX Cloud API</a>
      * @param underlying List of underlying symbols of the options.
      * @param expirationDate The expiration date of the options.
@@ -253,8 +287,10 @@ public class Reference {
     }
 
     /**
-     * <h2>Sectors Reference Data</h2>
-     * Returns an array of the sectors.
+     * Sectors Reference Data
+     *
+     * <p>Returns an array of the sectors.</p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_SECTORS#sectors-reference-data">IEX Cloud API</a>
      * @return A list of sectors.
      * @throws IOException If the request fails.
@@ -268,8 +304,13 @@ public class Reference {
     }
 
     /**
-     * <h2>Security Symbol Reference Data</h2>
-     * Returns an array of reference data for securities by symbol. Intraday price updates are tracked for these securities.
+     * Security Symbol Reference Data
+     *
+     * <p>
+     *     Returns an array of reference data for securities by symbol. Intraday price updates are tracked for these
+     *     securities.
+     * </p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA#security-symbols-reference-data">IEX Cloud API</a>
      * @return A list of security symbols.
      * @throws IOException If the request fails.
@@ -283,8 +324,10 @@ public class Reference {
     }
 
     /**
-     * <h2>Security Tags/Categories</h2>
-     * Returns an array of tags. Tags can be found on companies.
+     * Security Tags/Categories
+     *
+     * <p>Returns an array of tags. Tags can be found on companies.</p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_TAGS#security-tags-categories">IEX Cloud API</a>
      * @return A list of tags.
      * @throws IOException If the request fails.
@@ -298,8 +341,10 @@ public class Reference {
     }
 
     /**
-     * <h2>U.S. Exchanges</h2>
-     * Returns an array of U.S. exchanges.
+     * U.S. Exchanges
+     *
+     * <p>Returns an array of U.S. exchanges.</p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_EXCHANGES#u-s-exchanges">IEX Cloud API</a>
      * @return A list of U.S. exchanges.
      * @throws IOException If the request fails.
@@ -313,8 +358,10 @@ public class Reference {
     }
 
     /**
-     * U.S. Holidays & Trading Dates
-     * Returns the next trade date
+     * U.S. Holidays and Trading Dates
+     *
+     * <p>Returns the next trade date</p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/REF_DATA_DATES#u-s-holidays-trading-dates">IEX Cloud API</a>
      * @return The next trade date.
      * @throws IOException If the request fails.
@@ -330,11 +377,16 @@ public class Reference {
     }
 
     /**
-     * <h2>Autocomplete Search</h2>
-     * Returns an array of symbols up to the top 10 matches. Results will be sorted for relevancy. Search currently
-     * defaults to equities only, where the symbol returned is supported by endpoints listed under the Stocks category.
+     * Autocomplete Search
      *
-     * This endpoint is useful for creating an autocomplete search box.
+     * <p>
+     *     Returns an array of symbols up to the top 10 matches. Results will be sorted for relevancy. Search currently
+     *     defaults to equities only, where the symbol returned is supported by endpoints listed under the Stocks
+     *     category.
+     * </p>
+     *
+     * <p>This endpoint is useful for creating an autocomplete search box.</p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/autocomplete-search#autocomplete-search">IEX Cloud API</a>
      * @param fragment The fragment to search for.
      * @return A list of results.
@@ -350,8 +402,10 @@ public class Reference {
     }
 
     /**
-     * <h2>International Exchanges</h2>
-     * Returns an array of reference information about securities exchanges throughout the world.
+     * International Exchanges
+     *
+     * <p>Returns an array of reference information about securities exchanges throughout the world.</p>
+     *
      * @see <a href="https://iexcloud.io/docs/core/international-exchanges#international-exchanges">IEX Cloud API</a>
      * @return A list of international exchanges.
      * @throws IOException If the request fails.
